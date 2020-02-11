944af3024e45068c200d0d43ba44863fc3dd2da0
Stuart Leeks <stuart@leeks.net>
2020-02-11 17:12:37 +0000

    Add ls aliases and vscode extension helper

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,11 @@
 alias cls=clear
 alias md=mkdir
 alias cd..="cd .."
+
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+
+# helper to reset the marker files to reinstall extensions on reloading the devcontainer
+reset-vscode-extensions() { rm ~/.vscode-server/data/Machine/.installExtensionsMarker; rm ~/.vscode-server/data/Machine/.postCreateCommandMarker }
