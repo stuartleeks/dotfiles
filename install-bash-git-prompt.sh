4997d331bea60ed612abae61e27c109ac06e0dae
Stuart Leeks <stuart@leeks.net>
2020-02-19 12:17:55 +0000

    Add bash-git-prompt

---
+++ install-bash-git-prompt.sh
@@ -0,0 +1,16 @@
+set -e
+
+GIT_PROMPT_INSTALLED=$(cat ~/.bashrc | grep -q gitprompt.sh; echo $?)
+if [[ "$GIT_PROMPT_INSTALLED" == "0" ]]; then
+    echo "bash-git-prompt installed - exiting"
+    exit 0
+fi
+
+echo "Installing bash-git-prompt..."
+
+git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
+if [ -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
+    GIT_PROMPT_ONLY_IN_REPO=1
+    source $HOME/.bash-git-prompt/gitprompt.sh
+fi
+
--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
-# TODO - get current directory instead of hardcoding
-echo "source ~/dotfiles/aliases.sh" >> ~/.bashrc
+BASE_DIR=$(dirname "$0")
+
+echo "Adding aliases to .bashrc..."
+echo "source \"$BASE_DIR/aliases.sh\"" >> ~/.bashrc
+
+bash install-bash-git-prompt.sh
+
+echo "Done"
