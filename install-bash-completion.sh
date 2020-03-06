5af0772b78ff95c47eb92a1873058db94d91d6a6
Stuart Leeks <stuart@leeks.net>
2020-03-06 15:42:25 +0000

    Add source bash_completion script

--- install-bash-completion.sh
+++ install-bash-completion.sh
@@ -5,8 +5,10 @@
 if ! shopt -oq posix; then
   if [ -f /usr/share/bash-completion/bash_completion ]; then
     echo "completion file at /usr/share/bash-completion/bash_completion"
+    echo -e "source /usr/share/bash-completion/bash_completion\n" >> ~/.bashrc
   elif [ -f /etc/bash_completion ]; then
     echo "completion file at /etc/bash_completion"
+    echo -e "source /etc/bash_completion\n" >> ~/.bashrc
   else
     echo "bash-completion not found. Installing..."
     apt-get update
