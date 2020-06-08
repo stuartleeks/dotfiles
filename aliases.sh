2961c07bd990c156581099b68e734b49b714bec3
Stuart Leeks <stuart@leeks.net>
2020-06-08 08:54:06 +0100

    Add set-prompt

--- aliases.sh
+++ aliases.sh
@@ -9,3 +9,5 @@ alias l='ls -CF'
 
 # helper to reset the marker files to reinstall extensions on reloading the devcontainer
 reset-vscode-extensions() { rm ~/.vscode-server/data/Machine/.installExtensionsMarker; rm ~/.vscode-server/data/Machine/.postCreateCommandMarker; }
+
+function set-prompt() { echo -ne '\033]0;' $@ '\a'; }
