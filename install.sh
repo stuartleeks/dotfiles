8414a9fce301e0b33c69b616c34d5984e7302480
Stuart Leeks <stuart@leeks.net>
2020-08-22 10:30:17 +0100

    split install.sh/load.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+#
+# This script installs dependencies
+# It is intended to be run to set up the dotfiles
+# And adds to .bashrc to source the load.sh script 
+#
+
 BASE_DIR=$(dirname "$0")
 
 bash "$BASE_DIR/install-bash-completion.sh"
 
-bash "$BASE_DIR/git-aliases.sh"
+bash "$BASE_DIR/install-bash-git-prompt.sh"
 
-echo "Adding aliases to .bashrc..."
-echo -e "source \"$BASE_DIR/aliases.sh\"\n" >> ~/.bashrc
+# TODO - mostly want to set git-aliases in codespaces
+# Don't want to set on install as it breaks gitconfig integration in devcontainers
+# bash "$BASE_DIR/git-aliases.sh"
 
-
-bash install-bash-git-prompt.sh
+echo "Adding dotfiles loader to .bashrc..."
+echo -e "DOTFILES_FOLDER=\"$BASE_DIR\"\n" >> ~/.bashrc
+echo -e "source \"$BASE_DIR/load.sh\"\n" >> ~/.bashrc
 
 echo "Done"
---
+++ load.sh
@@ -0,0 +1,6 @@
+#
+# This script is intended to be sourced from .bashrc
+# It is added to .bashrc by install.sh
+#
+
+source "$DOTFILES_FOLDER/aliases.sh"
