a358e0a4d8b020cae4d8f90486f313c49998e7d5
Stuart Leeks <stuart@wfhome.localdomain>
2020-08-25 10:22:05 +0100

    Update free-mem to use --human if available

--- wsl/free-mem.sh
+++ wsl/free-mem.sh
@@ -1,7 +1,14 @@
-free -k
+#!/bin/bash
+
+FREE_ARGS="-k"
+
+if [[ $(free --help  2>&1 | grep -q human; echo $?) == 0 ]]; then
+    FREE_ARGS="-h"
+fi
+
+echo $FREE_ARGS | xargs free
 
 sync
 echo 3 > /proc/sys/vm/drop_caches
 
-free -k
-
+echo $FREE_ARGS | xargs free
