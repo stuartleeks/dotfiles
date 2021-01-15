60ffc81860797d826a4c8ff5e00b71c81f81e908
Stuart Leeks <stuart@leeks.net>
2021-01-15 12:37:20 +0000

    Add script to check az token before launching azbrowse

---
+++ wsl/azbrowse-with-token-check.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+SIGNED_IN=$(az account  get-access-token > /dev/null 2>&1; echo $?)
+if [[ "$SIGNED_IN" != "0" ]]; then
+    echo "No token or not signed in. Launching browser..."
+    # use hack from https://github.com/Azure/azure-cli/issues/14656#issuecomment-760794232
+    # Should be fine to remove once this is merged: https://github.com/Azure/azure-cli/pull/16556
+    DISPLAY=:0 az login > /dev/null 2>&1
+fi
+~/bin/azbrowse --subscription mpeck-stuartle
