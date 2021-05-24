4abf59a7a3687d0dfe49ff2ad80b9fb5ed9c39ec
Stuart Leeks <stuart@leeks.net>
2021-05-24 09:17:08 +0100

    Add ghrun alias

--- aliases/load.sh
+++ aliases/load.sh
@@ -50,4 +50,6 @@ fi
 
 if [[ $(command -v gh > /dev/null; echo $?) == 0 ]]; then
     eval "$(gh completion -s bash)"
+    
+    alias ghrun="gh run list | grep \$(git rev-parse --abbrev-ref HEAD) | cut -d$'\t' -f 8 | xargs gh run watch && notify-send 'Run finished'"
 fi
