b91b239a3f9d31bc5527a04c580c47d77403f230
Stuart Leeks <stuart@leeks.net>
2022-01-31 20:00:23 +0000

    Allow filtering by workflow in ghrun

--- aliases/ghrun.sh
+++ aliases/ghrun.sh
@@ -3,8 +3,56 @@ set -e
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+
+function show_usage() {
+    echo
+    echo "ghrun.sh"
+    echo
+    echo "Display the latest workflow execution for a PR - notifies on completion"
+    echo
+    echo -e "\t--workflow\t(Optional)The name of the workflow to query for."
+    echo
+    echo "Use 'git config stuartleeks.ghrun.workflow workflow_name' to set the default workflow for a git repo"
+    echo
+}
+
+# Set default values here
+workflow_name=
+
+# Process switches:
+while [[ $# -gt 0 ]]
+do
+    case "$1" in
+        --workflow)
+            workflow_name=$2
+            shift 2
+            ;;
+        *)
+            echo "Unexpected '$1'"
+            show_usage
+            exit 1
+            ;;
+    esac
+done
+
+if [[ -z $workflow_name ]]; then 
+    if [[ "0" == $(git config --get stuartleeks.ghrun.workflow > /dev/null; echo $?) ]]; then
+        workflow_name=$(git config --get stuartleeks.ghrun.workflow) # Use the value of stuartleeks.ghrun.workflow git config value unless overridden by workflow switch
+        if [[ -n $workflow_name ]]; then 
+            echo "Filtering to workflow '$workflow_name' (from stuartleeks.ghrun.workflow git config value)"
+        fi
+    fi
+else
+    echo "Filtering to workflow '$workflow_name'"
+fi
+
+workflow_arg=
+if [[ -n $workflow_name ]]; then
+    workflow_arg="--workflow '$workflow_name'"
+fi
+
 # Get the latest run for the current branch *update to use JSON output once implemented - see below)
-run_id=$(gh run list | grep $(git rev-parse --abbrev-ref HEAD) | cut -d$'\t' -f 7 | head -n 1)
+run_id=$(echo "$workflow_arg" | xargs gh run list | grep $(git rev-parse --abbrev-ref HEAD) | cut -d$'\t' -f 7 | head -n 1)
 
 # Watch the run progress
 gh run watch $run_id
