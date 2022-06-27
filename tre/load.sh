54a83a221380a2c9b0d78bcc5a557be6d3aa615e
Stuart Leeks <stuart@leeks.net>
2022-06-27 11:06:29 +0100

    Fix script path

--- tre/load.sh
+++ tre/load.sh
@@ -3,6 +3,6 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 alias tre-health="$dir/health.sh"
 alias tre-vmss-password="$dir/vmss-password.sh"
-alias tre-refid-for-pr="$dir/tre-refid-for-pr.sh"
+alias tre-refid-for-pr="$dir/refid-for-pr.sh"
 alias tre-swagger-ui="$dir/swagger-ui.sh"
 alias tre-run="$dir/run-watch.sh"
