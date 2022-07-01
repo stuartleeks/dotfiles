6a948a49bbe47ae0471ad5ba0da759fda87e255b
Stuart Leeks <stuart@leeks.net>
2022-07-01 07:13:06 +0100

    Simplify script
    Dev container test is made in the calling script

--- devcontainer/load.sh
+++ devcontainer/load.sh
@@ -1,16 +1,11 @@
-if [[ -n $DEV_CONTAINER ]]; then
-    ## In a dev container
-    ## add clip folder to path to override xsel/xclip with dev container versions!
-    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-    export PATH=$DIR/clip:$PATH
-fi
+## add clip folder to path to override xsel/xclip with dev container versions!
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export PATH=$DIR/clip:$PATH
+alias clip="xclip -i"
 
-if [[ -n $DEV_CONTAINER ]]; then
-    ## In a dev container
-    ## add toast folder to path to make notify-send/toast scripts available!
-    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-    export PATH=$DIR/toast:$PATH
-    export TOAST=toast
-fi
+## add toast folder to path to make notify-send/toast scripts available!
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export PATH=$DIR/toast:$PATH
+export TOAST=toast
 
 export AZBROWSE_SETTINGS_PATH="$DOTFILES_FOLDER/devcontainer/.azbrowse-settings.json"
