a69ba9ecb17f9eae8c4083ae0b2b23fe716d3a62
Stuart Leeks <stuart@leeks.net>
2022-08-19 12:20:05 +0100

    Add colour on dc install output

--- install-devcontainer.sh
+++ install-devcontainer.sh
@@ -4,11 +4,11 @@
 # This script sets DEV_CONTAINER=1 and then calls install.sh
 #
 
-echo "========================== dotfiles install - start ========================== "
+echo -e "\033[0;93m========================== dotfiles install - start ==========================\033[0m"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export DEV_CONTAINER=1
 
 $DIR/install.sh
 
-echo "========================== dotfiles install - end   ========================== "
+echo -e "\033[0;93m========================== dotfiles install - end   ==========================\033[0m"
