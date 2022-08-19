5c58f8bb81cd749340905c219085de53a4d76764
Stuart Leeks <stuart@leeks.net>
2022-08-19 12:13:58 +0100

    Add dc install output

--- install-devcontainer.sh
+++ install-devcontainer.sh
@@ -4,7 +4,11 @@
 # This script sets DEV_CONTAINER=1 and then calls install.sh
 #
 
+echo "========================== dotfiles install - start ========================== "
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export DEV_CONTAINER=1
 
 $DIR/install.sh
+
+echo "========================== dotfiles install - end   ========================== "
