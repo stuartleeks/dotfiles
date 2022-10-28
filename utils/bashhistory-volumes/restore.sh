17ccf286fa6bca31e8c228e1663ec5a6e0c16f61
Stuart Leeks <stuart@leeks.net>
2022-10-28 23:25:15 +0100

    Add scripts for backup/restore of docker volumes for bash history

---
+++ utils/bashhistory-volumes/restore.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+
+ls volume_bak | while read backup_filename
+do
+	base_filename=$(basename --suffix .tar $backup_filename)
+	echo $base_filename
+
+	echo "Restoring $base_filename"
+	docker run --rm -v $script_dir/volume_bak:/backup -v ${base_filename}:/bhvolume ubuntu bash -c "cd /bhvolume && tar xvf /backup/$base_filename.tar > /dev/null"
+done
