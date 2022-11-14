661e98872adc1f7f8059bc9d2623411b2b094e7b
Stuart Leeks <stuart@leeks.net>
2022-11-14 16:44:15 +0000

    Reverse diff semantics on gco alias

--- aliases/load.sh
+++ aliases/load.sh
@@ -74,7 +74,7 @@ fi
 
 if [[ $(command -v fzf > /dev/null; echo $?) == 0 ]]; then
     # https://mastodon.social/@elijahmanor/109320029491309392
-    alias gco="git branch --sort=-committerdate | fzf --preview=\"git diff --color=always '{1}'\" --header \"git checkout\" | xargs git checkout"
+    alias gco="git branch --sort=-committerdate | fzf --preview=\"git diff --color=always $(git rev-parse HEAD) '{1}'\" --header \"git checkout\" | xargs git checkout"
     if [[ $(command -v fd > /dev/null; echo $?) == 0 ]]; then
         alias fdf="fd --type f --hidden --exclude .git"
         alias fdff="fd --type f --hidden --exclude .git | fzf"
