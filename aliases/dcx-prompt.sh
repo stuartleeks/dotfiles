9ce586a21f1d28da9e623e6f4ceb5ca0f7d233b3
Stuart Leeks <stuart@leeks.net>
2023-07-11 20:33:26 +0100

    Fix fzf resolution for dcx-prompt and suppress update check

--- aliases/dcx-prompt.sh
+++ aliases/dcx-prompt.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
+export PATH="$PATH:$HOME/.fzf/bin"
 
 dc_name=$(~/.local/bin/devcontainerx list | fzf --preview="~/.local/bin/devcontainerx show --name '{1}'" --header "devcontainer exec") 
 
-~/.local/bin/devcontainerx exec --name "$dc_name"
+DEVCONTAINERX_SKIP_UPDATE=1 ~/.local/bin/devcontainerx exec --name "$dc_name"
 
