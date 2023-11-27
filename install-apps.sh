266568255c0509a7e4f73f42d6f2163e1968762d
Stuart Leeks <stuart@leeks.net>
2023-11-27 21:19:57 +0000

    update node installation

--- install-apps.sh
+++ install-apps.sh
@@ -45,7 +45,16 @@ else
 fi
 
 if [[ $(command -v npm > /dev/null; echo $?) == 1 ]]; then
-    ~/utils/tools-install/node.sh
+    # ~/utils/tools-install/node.sh 18
+    # https://github.com/nodesource/distributions#ubuntu-versions
+    sudo apt-get update
+    sudo apt-get install -y ca-certificates curl gnupg
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+    NODE_MAJOR=18
+    echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+    sudo apt-get update
+    sudo apt-get install nodejs -y
 else
     echo "✅ node/npm already installed"
 fi
